5e7f669627613127186cf1afc2122714b3f46b61
mononoken <ken.maeshima@pm.me>
2023-11-20 04:05:50 -0800

    Create notes_helper_spec

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -6,13 +6,19 @@ module NotesHelper
       process_custom_tags("<p>#{text.strip}</p>\n")
     end
 
-    def list_item(text, params)
+    def list_item(text, list_type)
       "<li>#{process_custom_tags(text)}</li>\n"
     end
 
     def header(text, level)
       "<h#{level}>#{process_custom_tags(text)}</h#{level}>"
     end
+
+    def link(url, title, content)
+      %(<a href="#{url}">#{content}</a>)
+    end
+
+    private
 
     def internal_link(note_name)
       note = Note.find_by(name: note_name)
@@ -25,12 +31,6 @@ module NotesHelper
       link(url, title, content)
     end
 
-    def link(link, title, content)
-      "<a href='#{link}'>#{content}</a>"
-    end
-
-    private
-
     def process_custom_tags(text)
       matches = text.to_enum(:scan, /\[\[(.*?)\]\]/).map { Regexp.last_match }
 
@@ -41,6 +41,45 @@ module NotesHelper
       text
     end
   end
+
+  # class InternalLinksProcessor
+  #   attr_reader :note_name, :note_class
+  #
+  #   def initialize(note_name, note_class = Note)
+  #     @note_name = note_name
+  #     @note_class = note_class
+  #   end
+  #
+  #   def process_custom_tags(text)
+  #     matches = text.to_enum(:scan, /\[\[(.*?)\]\]/).map { Regexp.last_match }
+  #
+  #     if matches.any?
+  #       matches.each { |match| text.sub!(match[0], internal_link(match[1])) }
+  #     end
+  #
+  #     text
+  #   end
+  #
+  #   def internal_link
+  #     return "[[#{note_name}]]" unless note
+  #
+  #     url = Rails.application.routes.url_helpers.note_path(note)
+  #     title = note.name
+  #     content = note.name
+  #
+  #     link(url, title, content)
+  #   end
+  #
+  #   def link(url, title, content)
+  #     %(<a href="#{url}">#{content}</a>)
+  #   end
+  #
+  #   private
+  #
+  #   def note
+  #     note_class.find_by(note_name)
+  #   end
+  # end
 
   def markdown(input)
     # Redcarpet docs recommend instantiating renderer object first before use.
